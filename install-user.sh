f15184da6632af2aa23ef0db1132c2343dabf8fb
dacctal <120422854+dacctal@users.noreply.github.com>
2025-10-26 05:05:22 -0400

    0.0.8-ergoterm major improvements to command syntax and permissions

--- install-user.sh
+++ install-user.sh
@@ -1,8 +1,6 @@
-nim c -d:release -o:pkgit src/pkgit.nim &&
-  clear &&
-  mv pkgit ~/.local/bin/pkgit
-  if [ ! -f "$HOME"/.config/pkgit ]; then
-    mkdir -p "$HOME"/.config/pkgit
+nim c -d:release -o:pkgit src/pkgit.nim && mv pkgit ~/.local/bin/pkgit
+if [ ! -f "$HOME"/.config/pkgit ]; then
+  mkdir -p "$HOME"/.config/pkgit
   echo """[general]
-user-level = true""" >> "$HOME"/.config/pkgit/config.toml
-  fi
+  user-level = true""" >> "$HOME"/.config/pkgit/config.toml
+fi
